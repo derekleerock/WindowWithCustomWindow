6248b862d8d9ca0993934d050b78777b108d97c1
Derek Lee <derek@sunrisingappdev.com>
2022-12-02 16:16:14 +0900

    Initial Commit

---
+++ WindowWithCustomWindow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WindowWithCustomWindow
+//
+//  Created by Derek Lee on 2022/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
